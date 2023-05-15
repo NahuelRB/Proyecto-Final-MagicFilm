5c737319c1577ba5de51a67af7d00e3001a69e5e
Yazuline <ingeniera-yazuline@hotmail.com>
2023-05-15 15:09:46 -0500

    se adiciona html al header y se crea archivo style css

--- frontEnd/magicFilm/src/components/layout/header/HeaderLayout.jsx
+++ frontEnd/magicFilm/src/components/layout/header/HeaderLayout.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import "./HeaderStyle.css"
 const HeaderLayout = () => {
   return (
-    <div>HeaderLayout</div>
+    <div  className='Navbar'>
+
+    <span className='nav-logo'>MAGIC</span>
+    <div className='nav-items'>
+      <a href='#'><button>Iniciar sesión</button></a>
+      <a href='#'><button>Crear cuenta</button></a>
+    </div>
+    <div className='nav-toggle'>
+
+    </div>
+
+    </div>
   )
 }
 
