3e54528cb723f5039f4e45f52daea45ce6609d8f
Yazuline <ingeniera-yazuline@hotmail.com>
2023-05-15 18:15:50 -0500

    header  estilo css resposive

--- frontEnd/magicFilm/src/components/layout/header/HeaderLayout.jsx
+++ frontEnd/magicFilm/src/components/layout/header/HeaderLayout.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./HeaderStyle.css"
 const HeaderLayout = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div  className='Navbar'>
 
     <span className='nav-logo'>MAGIC</span>
-    <div className='nav-items'>
+    <div className={`nav-items ${isOpen && "open"}`}>
+     
       <a href='#'><button>Iniciar sesión</button></a>
       <a href='#'><button>Crear cuenta</button></a>
     </div>
-    <div className='nav-toggle'>
+    <div className={`nav-toggle ${isOpen && "open"}`}
+     onClick={()=> setIsOpen(!isOpen)}>
+      <div className='bar'></div>
 
     </div>
 
