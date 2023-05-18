1a5897e68d202b225ea99034684a93966dc4349d
estebanvasquex <estebanvasquex@gmail.com>
2023-05-17 20:37:14 -0500

    Estructura sección recomendados se itera con archivo films.json

--- frontEnd/magicFilm/src/components/pages/home/Home.jsx
+++ frontEnd/magicFilm/src/components/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SearchMoviesContainer from '../../common/searchMovies/SearchMoviesContainer'
+import RecommendedMoviesContainer from '../../common/recommendedMovies/RecommendedMoviesContainer'
 
 
 const Home = () => {
   return (
     <div>
       <SearchMoviesContainer/>
+      <RecommendedMoviesContainer/>
     </div>
   )
 }
