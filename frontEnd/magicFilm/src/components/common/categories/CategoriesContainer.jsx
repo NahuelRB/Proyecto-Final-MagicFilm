088e44c0dad5559212742d8fd9e6349ac226d7b1
jennyalexandraca <yenialeza@gmail.com>
2023-05-21 17:41:29 -0500

    Categorías home con responsive

---
+++ frontEnd/magicFilm/src/components/common/categories/CategoriesContainer.jsx
@@ -0,0 +1,12 @@
+import Categories from './Categories'
+
+
+const CategoriesContainer = () => {
+return (
+    <div>
+        <Categories/>
+    </div>
+    )
+}
+
+export default CategoriesContainer
--- frontEnd/magicFilm/src/components/pages/home/Home.jsx
+++ frontEnd/magicFilm/src/components/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import SearchMoviesContainer from '../../common/searchMovies/SearchMoviesContainer'
 import RecommendedMoviesContainer from '../../common/recommendedMovies/RecommendedMoviesContainer'
-
+import CategoriesContainer from '../../common/categories/CategoriesContainer'
 
 const Home = () => {
   return (
     <div>
       <SearchMoviesContainer/>
+      <CategoriesContainer/>
       <RecommendedMoviesContainer/>
     </div>
   )
