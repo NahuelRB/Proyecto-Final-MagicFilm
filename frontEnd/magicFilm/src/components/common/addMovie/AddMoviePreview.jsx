648a2f9346e998e38b3c8b92f6a675f630b450dc
Mayra Torres <mayratorresas@unimagdalena.edu.co>
2023-05-22 08:03:19 -0500

    card Movie Component

--- frontEnd/magicFilm/src/components/common/addMovie/AddMoviePreview.jsx
+++ frontEnd/magicFilm/src/components/common/addMovie/AddMoviePreview.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./addMovie.css";
-
+import CardMovie from "../cardMovie/cardMovie";
 const AddMovie = ({ state, setState, initialState }) => {
   let { title, gender, release, image, link } = state;
   const handleReset = () => {
@@ -9,35 +9,7 @@ const AddMovie = ({ state, setState, initialState }) => {
   return (
     <div className="container-preview">
       <h2 className="title">Previsualización película agregada</h2>
-      <div className="card-preview">
-        <img src={image} alt="" className="card-movie-img" />
-        <div className="card-movie-info">
-          <h3
-            style={{
-              fontWeight: 700,
-            }}
-          >
-            Detalle de la película
-          </h3>
-          <br />
-
-          <p>
-            <b className="card-movie-title">Titulo: </b>
-            {title}
-          </p>
-
-          <p>
-            <b className="card-movie-title">Fecha de estreno: </b>
-            {release}
-          </p>
-          <p>
-            <b className="card-movie-title">Género: </b>
-            {gender}
-          </p>
-          <b className="card-movie-title">Trailer: </b>
-          {link && <a href={link}>Ver Trailer</a>}
-        </div>
-      </div>
+      <CardMovie state={state} />
       {(title || gender || release || image || link) && (
         <button className="create" onClick={handleReset}>
           Cancelar
