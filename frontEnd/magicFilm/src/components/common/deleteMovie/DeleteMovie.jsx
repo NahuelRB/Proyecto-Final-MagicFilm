9afe588f5dc9939af5a2d10d13cd09431490daa7
Carolina Taglioli <caro.taglioli@gmail.com>
2023-05-22 20:52:56 -0300

    delete v2

--- frontEnd/magicFilm/src/components/common/deleteMovie/DeleteMovie.jsx
+++ frontEnd/magicFilm/src/components/common/deleteMovie/DeleteMovie.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieDetailContainer from "../../pages/movieDetail/MovieDetailContainer";
 import SearchMoviesContainer from "../searchMovies/SearchMoviesContainer";
+import { getMovie, deleteMovie } from "../../../services/movieServices";
 import "./deleteMovie.css";
 
 const DeleteMovie = () => {
+  const [dataSearch, setDataSearch] = useState("");
+  const [data, setData] = useState(null);
+
+  const handleSearch = async (e) => {
+    setData(await getMovie(e));
+  };
+
+  console.log(data?.data[0].id);
+
   return (
     <div>
       <form action="submit" className="formulario-delete-movie">
         <div className="titulo-delete-movie">Eliminar película</div>
 
-        <SearchMoviesContainer />
+        <SearchMoviesContainer
+          setDataSearch={setDataSearch}
+          dataSearch={dataSearch}
+          handleSearch={handleSearch}
+        />
 
-        <MovieDetailContainer/>
+        <MovieDetailContainer />
 
-        <button className="button-delete-movie">Eliminar película</button>
+        <button
+          onClick={(e) => {
+            deleteMovie(data?.data[0].id);
+            e.preventDefault();
+          }}
+          className="button-delete-movie"
+        >
+          Eliminar película
+        </button>
       </form>
     </div>
   );
