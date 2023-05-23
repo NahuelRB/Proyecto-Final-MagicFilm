8c13781c871cdcdc24a328752c6c4e4d95285e1c
Mayra Torres <mayratorresas@unimagdalena.edu.co>
2023-05-22 21:12:49 -0500

    fix categories

--- frontEnd/magicFilm/src/pages/Home.jsx
+++ frontEnd/magicFilm/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import SearchMoviesContainer from "../components/common/searchMovies/SearchMoviesContainer";
 import RecommendedMoviesContainer from "../components/common/recommendedMovies/RecommendedMoviesContainer";
+import CategoriesContainer from "../components/common/categories/CategoriesContainer";
 
 const Home = () => {
   return (
     <Layout>
       <SearchMoviesContainer />
+      <CategoriesContainer />
       <RecommendedMoviesContainer />
     </Layout>
   );
