4ba26f20ce5237e12786c51226bf85fb775dcf3d
jennyalexandraca <yenialeza@gmail.com>
2023-05-24 22:00:26 -0500

    Categorías en el front ajustadas: se agregó la 4ta y se actualizó responsive

--- frontEnd/magicFilm/src/components/common/categories/Categories.jsx
+++ frontEnd/magicFilm/src/components/common/categories/Categories.jsx
@@ -6,29 +6,38 @@ const Categories = () => {
         <div className='container-categories'>
             <div className='container-images'>
                 <a href="#">
-                    <img className="zoom" src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684370572/estrenos_hish13.jpg" alt="estrenos"/>
+                    <img className="zoom" src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684978863/Estrenos_etsgfy.png" alt="estrenos"/>
                 </a>
                 <a href="#">
-                    <img className="zoom" src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684370572/preventa_qarwow.jpg" alt="películas en preventa"/>
+                    <img className="zoom" src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684978919/Preventa_p2hofm.png" alt="películas en preventa"/>
                 </a>
                 <a href="#">
-                    <img className="zoom" src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684370573/cartelera_fed7av.jpg" alt="películas en cartelera"/>
+                    <img className="zoom" src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684978967/Cartelera_eerfch.png" alt="películas en cartelera"/>
+                </a>
+                <a href="#">
+                    <img className="zoom" src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684979172/Promociones_h5tjv8.png" alt="promociones"/>
                 </a>      
             </div>
         </div>
-         <div className='nav-menu'>
-         <a href="#">
-                 <img src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684697721/Estrenos_l0gwwa.png" alt="estrenos"/>
-             </a>
-             <a href="#">
-                 <img src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684697721/Preventa_mcttwl.png" alt="películas en preventa"/>
-             </a>
-             <a href="#">
-                 <img src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684697721/Cartelera_iw60vx.png" alt="películas en cartelera"/>
-             </a>     
-         </div>
+        <div className='nav-menu'>
+        <a href="#">
+                <img src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684697721/Estrenos_l0gwwa.png" alt="estrenos"/>
+            </a>
+            <a href="#">
+                <img src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684697721/Preventa_mcttwl.png" alt="películas en preventa"/>
+            </a>
+            <a href="#">
+                <img src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684697721/Cartelera_iw60vx.png" alt="películas en cartelera"/>
+            </a>
+            <a href="#">
+                <img src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684697721/Cartelera_iw60vx.png" alt="películas en cartelera"/>
+            </a>
+            <a href="#">
+                <img src="https://res.cloudinary.com/dtzs6lm4b/image/upload/v1684977985/promocionesMobile_jyofic.png" alt="promociones"/>
+            </a>            
+        </div>
 
-         </>
+        </>
     )
 }
 
