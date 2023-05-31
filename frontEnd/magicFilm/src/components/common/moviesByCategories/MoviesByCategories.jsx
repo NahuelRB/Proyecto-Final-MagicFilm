01037b3c725d91da5c1621764ae47eec6c96844c
jennyalexandraca <yenialeza@gmail.com>
2023-05-30 23:23:24 -0500

    Se actualizó la api incluyendo la key de categorías, así mismo el componente de películas recomendadas para actualizar el acceso a la api y el de MoviesByCategories

--- frontEnd/magicFilm/src/components/common/moviesByCategories/MoviesByCategories.jsx
+++ frontEnd/magicFilm/src/components/common/moviesByCategories/MoviesByCategories.jsx
@@ -1,27 +1,28 @@
 import { Link, useParams } from "react-router-dom";
 
-const MoviesByCategories = ({ dataMovie }) => {
+const MoviesByCategories = (props) => {
+    const {dataMovie} = props
     const { category_id } = useParams();
 
     const filteredMovies = dataMovie.filter(
-        (movie) => movie.id === parseInt(category_id)
+        (movies) => movies.movie_id === parseInt(category_id)
     );
 
     return (
         <div className="container-home-card">
-            {filteredMovies.map((movie) => (
-                <div key={movie.id} className="card-container-search-movies">
-                    <img src={movie.image} alt="" className="img-search-movies" />
+            {filteredMovies.map((movies) => (
+                <div key={movies.movie_id} className="card-container-search-movies">
+                    <img src={movies.image} alt="" className="img-search-movies" />
                     <br />
                     <div className="info-search-movie">
                         <h4>
-                            Estreno: <p>{movie.release_date}</p>
+                            Estreno: <p>{movies.release_date}</p>
                         </h4>
                         <h4>
-                            Género: <p>{movie.gender}</p>
+                            Género: <p>{movies.gender.gender_id}</p>
                         </h4>
                         <br />
-                        <Link to={`/details/${movie.id}`}>
+                        <Link to={`/details/${movies.movie_id}`}>
                             <button className="button-search-detail">Ver más</button>
                         </Link>
                     </div>
