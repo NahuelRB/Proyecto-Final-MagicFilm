e623756cdc8c5938e9c7d730748db827a2624fa7
jennyalexandraca <yenialeza@gmail.com>
2023-05-31 23:07:22 -0500

    Se aplicaron estilos a los botones del filtro de categorías

--- frontEnd/magicFilm/src/components/common/searchCategories/SearchCategories.jsx
+++ frontEnd/magicFilm/src/components/common/searchCategories/SearchCategories.jsx
@@ -1,45 +1,54 @@
-// import "./SearchCategories.css";
-
+import "./searchCategories.css";
 import { useState } from "react";
 
 const SearchCategories = ({ handleCategoryFilter }) => {
-  const [activeButton, setActiveButton] = useState("");
+  const [activeButtonCategory, setActiveButtonCategory] = useState("");
 
   const handleCategoryClick = (category) => {
-    setActiveButton(category);
+    setActiveButtonCategory(category);
     handleCategoryFilter(category);
   };
 
   return (
-    <div>
-      <h2>Filtra por categoría</h2>
-      <div>
+    <div className="container-filter-search-categories">
+      {/* <h2 className="title-category-filter">Filtra por categoría</h2> */}
+      <div className="container-buttons-search-categories">
         <button
-          className={activeButton === "Estrenos" ? "active" : ""}
+          className={`button-search-categories ${
+            activeButtonCategory === "Estrenos" ? "activeButtonCategory" : ""
+          }`}
           onClick={() => handleCategoryClick("Estrenos")}
         >
           Estrenos
         </button>
         <button
-          className={activeButton === "Preventa" ? "active" : ""}
+          className={`button-search-categories ${
+            activeButtonCategory === "Preventa" ? "activeButtonCategory" : ""
+          }`}
           onClick={() => handleCategoryClick("Preventa")}
         >
           Preventa
         </button>
         <button
-          className={activeButton === "Cartelera" ? "active" : ""}
+          className={`button-search-categories ${
+            activeButtonCategory === "Cartelera" ? "activeButtonCategory" : ""
+          }`}
           onClick={() => handleCategoryClick("Cartelera")}
         >
           Cartelera
         </button>
         <button
-          className={activeButton === "Promociones" ? "active" : ""}
+          className={`button-search-categories ${
+            activeButtonCategory === "Promociones" ? "activeButtonCategory" : ""
+          }`}
           onClick={() => handleCategoryClick("Promociones")}
         >
           Promociones
         </button>
         <button
-          className={activeButton === "Todas" ? "active" : ""}
+          className={`button-search-categories ${
+            activeButtonCategory === "Todas" ? "activeButtonCategory" : ""
+          }`}
           onClick={() => handleCategoryClick("Todas")}
         >
           Ver todas
