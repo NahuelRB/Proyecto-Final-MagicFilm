df12d1843d85388402bcdf214ed4668571c75865
Mayra Torres <mayratorresas@unimagdalena.edu.co>
2023-06-01 03:25:06 -0500

    fix recommended movies

--- frontEnd/magicFilm/src/components/common/recommendedMovies/RecommendedMovies.jsx
+++ frontEnd/magicFilm/src/components/common/recommendedMovies/RecommendedMovies.jsx
@@ -17,7 +17,7 @@ const recommendedMovies = (props) => {
                   Estreno: <p>{movies.release_date}</p>
                 </h4>
                 <h4>
-                  Género: <p>{movies.gender.name}</p>
+                  Género: <p>{movies.gender}</p>
                 </h4>
 
                 <br />
