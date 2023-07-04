b24e9d6e913dc30e6c14a856f26b9c9945cdad7f
Mayra Torres <mayratorresas@unimagdalena.edu.co>
2023-07-04 13:59:57 -0500

    change max space

--- frontEnd/magicFilm/vite.config.js
+++ frontEnd/magicFilm/vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { //add this property
+  build: {
     sourcemap: true,
-},
+  },
+  server: {
+    // Ajusta el límite de memoria asignado a Node.js (en megabytes)
+    // El valor predeterminado es 4096 (4 GB)
+    // Puedes ajustar este valor según tus necesidades
+    memoryLimit: 8192, // 8 GB
+  },
   define: {
     global: 'window',
   },
